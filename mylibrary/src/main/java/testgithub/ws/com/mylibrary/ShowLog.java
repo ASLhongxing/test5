359a86adb48e0eca3204d0c78ca871f07a872eda
zhanghongxing <15030098575@163.com>
2018-07-24 17:38:24 +0800

    Initial commit

---
+++ mylibrary/src/main/java/testgithub/ws/com/mylibrary/ShowLog.java
@@ -0,0 +1,16 @@
+package testgithub.ws.com.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018/7/24.
+ */
+
+public class ShowLog {
+
+    public void showLog(Context context){
+        Toast.makeText(context, "123456", Toast.LENGTH_SHORT).show();
+    }
+
+}
